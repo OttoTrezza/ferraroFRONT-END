dfb4c74d7cb41eae4480b0bf3dff7b495657e10a
OttoTrezza <40862141+OttoTrezza@users.noreply.github.com>
2019-12-03 15:22:14 -0300

    areas-p.component cargado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AreasPComponent } from './components/areas-p/areas-p.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AreasPComponent
   ],
   imports: [
     BrowserModule
