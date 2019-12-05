61cbf065faeddc20549dcccb9e280c9c84246e22
OttoTrezza <40862141+OttoTrezza@users.noreply.github.com>
2019-12-05 14:41:03 -0300

    desde cero

---
+++ src/app/components/eji/eji.components.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-eji',
+  templateUrl: './eji.component.html',
+  styleUrls: ['./eji.component.css']
+})
+export class EjiComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+}
