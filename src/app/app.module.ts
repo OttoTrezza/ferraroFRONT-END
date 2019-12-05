9ac2c9324cc479187c2e35ea641f093398a7d0a8
OttoTrezza <40862141+OttoTrezza@users.noreply.github.com>
2019-12-05 14:49:31 -0300

    DESDE CERO

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AreasPComponent } from './components/areas-p/areas-p.component';
+import { EjiComponent } from './components/eji/eji.component';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    AreasPComponent
+    AreasPComponent,
+    EjiComponent
   ],
   imports: [
     BrowserModule
