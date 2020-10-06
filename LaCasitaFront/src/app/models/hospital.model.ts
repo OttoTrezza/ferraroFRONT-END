a4351ac4c9b02ae7b22b8938a3a3a99741a050ec
OttoTrezza <40862141+OttoTrezza@users.noreply.github.com>
2020-10-06 19:53:48 -0300

    restaurado

--- LaCasitaFront/src/app/models/hospital.model.ts
+++
@@ -1,9 +0,0 @@
-
-export class Hospital {
-    constructor(
-        public nombre: string,
-        public img?: string,
-        public usuario?: any,
-        public _id?: string
-    ) {}
-}
--- LaCasitaFront/src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: false,
-     wsUrl: 'https://ferraroeji.herokuapp.com/' // http://localhost:300/
-};
